313b35ce85c4a65b867f443690b36e4ee6a4cc38
Cody Grinstead <yodacodyone@gmail.com>
2018-08-30 09:49:03 -0600

    hello world maybe php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+	$name = cody;
+	$header = "<html><title>the thing </title>";
+	$body = "<body>Hello World </body>";
+	$footer= "</html>"
+	echo $header;
+	echo $body;
+	echo $footer;
+
+?>
